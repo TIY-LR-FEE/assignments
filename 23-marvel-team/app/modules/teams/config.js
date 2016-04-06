06bb48408f81659ad0cc7d7c772da18d8a3a1f7c
Jonathan Barket <jbarket@sleepunit.com>
2016-04-06 13:18:55 -0500

    Fixed typo

--- 23-marvel-team/app/modules/teams/config.js
+++ 23-marvel-team/app/modules/teams/config.js
@@ -9,7 +9,7 @@
   STEP 1: Use $stateProvider.state to add a single route called
           new_team that points to /teams/new
 
-          It should use the TeamController and call it teamCtrl.
+          It should use the TeamsController and call it teamCtrl.
 
           It should use the ./view.html template. Don't forget,
           we use require() to include templates!
