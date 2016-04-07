3f4798a7d145535e574ad478d889ee8d92a7e78c
Jonathan Barket <jbarket@sleepunit.com>
2016-04-07 13:16:24 -0500

    Fixed typo
    Error in view location

--- 24-marvel-crud/app/modules/teams/config.js
+++ 24-marvel-crud/app/modules/teams/config.js
@@ -7,28 +7,28 @@
 
           It should use the TeamsController and call it teamsCtrl.
 
-          It should use the ./view/teams.html template.
+          It should use the ./views/teams.html template.
 
   STEP 2: Use $stateProvider.state to add a route called
           new_team that points to /new
 
           It should use the NewTeamController and call it newCtrl.
 
-          It should use the ./view/new_team.html template.
+          It should use the ./views/new_team.html template.
 
   STEP 3: Use $stateProvider.state to add a route called
           edit_team that points to /:id/edit
 
           It should use the EditTeamController and call it editCtrl.
 
-          It should use the ./view/edit_team.html template.
+          It should use the ./views/edit_team.html template.
 
   STEP 4: Use $stateProvider.state to add a route called
           team that points to /:id
 
           It should use the TeamController and call it teamCtrl.
 
-          It should use the ./view/team.html template.
+          It should use the ./views/team.html template.
 
 
 */
